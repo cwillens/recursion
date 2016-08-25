f00896d13e35d77d3c676ea47efbfea9824c39ae
Charlotte Willens <charwillens@gmail.com>
2016-08-25 13:19:56 -0700

    implemented for primitives and arrays, next up other objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,27 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "number"|| typeof obj==="boolean"|| obj===null) {
+  	return String(obj);
+  }
+  else if (typeof obj === "string") {
+  	return "\""+obj+"\"";
+  }
+  else if (typeof obj === "undefined" || typeof obj==="symbol" || typeof obj==="function") {
+  	return undefined;
+  }
+  else if (if Array.isArray(obj)) {
+	var result = "[";
+	for (var i=0; i<obj.length; i++) {
+		if (i<obj.length-1) {
+			if (stringifyJSON(obj[i])===undefined) result = result + "null" + ",";
+			else result = result + stringifyJSON(obj[i]) + ",";
+		}
+		else {
+			if (stringifyJSON(obj[i])===undefined) result = result + "null" + "]";
+			else result = result + stringifyJSON(obj[i]) + "]";
+		}
+	}
+	return result;
+  }
 };

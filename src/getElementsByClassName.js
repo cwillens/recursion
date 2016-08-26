7746de73469b9fcd9d1a428678ffaf59b5ff9770
Charlotte Willens <charwillens@gmail.com>
2016-08-25 19:29:27 -0700

    implemented getElementsbyClassName. I used document.body, element.childNodes, and element.classList, and recursion, though I recursed on a helper function that I wrote rather than the getElementsbyClassName function. Hopefully that's okay

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,32 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  /* var results=[];
+  1. look at all nodes in document and see if they have class classname
+  		to do this: a) Document.body.childNodes gives an array of all of the elements in the body
+  		.classList - this gives an array of the classes for an element. needs to be done on an element,
+  		not an array b) so if we do classList on each element in doc.body.childnodes we can test if our
+  		class is in the list and if so push it into the results array.
+   */
+   
+   var results = [];
+   var checkForClass = function(node) {
+
+   		if (node.childNodes.length===0) {
+   		    if (node.classList===undefined) return;
+            else if (node.classList.contains(className)) {
+                results.push(node);
+   		        return;	
+            }		
+   		}
+   		else {
+   			if (node.classList.contains(className)) results.push(node);
+   			for (var i=0; i<node.childNodes.length; i++) {
+   				checkForClass(node.childNodes[i]);
+   			}
+   		}
+   };
+
+   checkForClass(document.body);
+   return results;
 };
